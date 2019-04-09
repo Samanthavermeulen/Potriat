85eefdd25234033f507d284a7a3b4c0c0550ef72
Samanthavermeulen <samantha.vermeulen@hotmail.com>
2019-04-09 12:14:08 +0200

    Udemy course exercise make potret

---
+++ Exercise03/Images.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Exercise03
+{
+    public class Images
+    {
+     
+
+        public void Potrait()
+        {
+
+                Console.WriteLine("This is a landscape");
+            
+        }
+
+        public void Landscape()
+        {
+                Console.WriteLine("This is a Potrait");
+            
+        }
+    }
+}
---
+++ Exercise03/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newImages = new Images();
+            
+            Console.WriteLine("Enter the width");
+            var width= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Height");
+            var height = Convert.ToInt32(Console.ReadLine());
+
+            if (width > 200 && height < 150)
+            {
+                newImages.Potrait();
+            }
+            else
+            {
+                newImages.Landscape();
+            }
+        }
+    }
+}
